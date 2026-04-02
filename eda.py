d7b8755985a7b15dc0da1f37a0e8b151e2cb690d
s74289335-cmyk <s74289335@gmail.com>
2026-04-03 01:09:03 +0530

    Delete eda.py

--- eda.py
+++
@@ -1,9 +0,0 @@
-from sklearn.ensemble import IsolationForest
-
-def detect_anomalies(df):
-    num_cols = df.select_dtypes(include=['float64', 'int64']).columns
-    
-    model = IsolationForest(contamination=0.01)
-    df['anomaly'] = model.fit_predict(df[num_cols])
-    
-    return df
